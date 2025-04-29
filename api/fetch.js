599d33ed7fb0b10ab506cccdaec7045dc8973f0b
dannysonfire <dannyp32008@gmail.com>
2025-04-28 22:39:14 -0400

    Update fetch.js

--- api/fetch.js
+++ api/fetch.js
@@ -3,6 +3,10 @@ export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end(); // Allow preflight
+  }
+
   const { q } = req.query;
 
   if (!q) {
